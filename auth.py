c0388252fad0a39f3096703758ade6173db5b914
Dakota Hester <hesterdakota@gmail.com>
2021-10-07 21:23:06 -0500

    Fixed revoke, added API playground, tidied up auth

--- auth.py
+++ auth.py
@@ -16,6 +16,9 @@ passthrough = "test" # This is used for something or rather
             # Shouldn't be an issue in prod
 
 def authorize(callback_uri):
+    print("auth.py: authorize(): authorize() called, beginning OAuth 2.0 authorization")
+
+    print("auth.py: authorize(): creating `flow` object...")
     # Create flow instance to manage the OAuth 2.0 Authorization Grant Flow steps.
     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
         CLIENT_SECRETS_FILE_LOC, scopes=SCOPES)
@@ -25,46 +28,55 @@ def authorize(callback_uri):
     # value doesn't match an authorized URI, you will get a 'redirect_uri_mismatch'
     # error.
     flow.redirect_uri = callback_uri
+    print("auth.py: authorize(): `flow` object created")
 
+    print("auth.py: authorize(): obtaining authorizatiun_url...")
     authorization_url = flow.authorization_url(
         # Enable offline access so that you can refresh an access token without
         # re-prompting the user for permission. Recommended for web server apps.
         access_type='offline',
         # Enable incremental authorization. Recommended as a best practice.
         include_granted_scopes='true')
-    
+        
+    print("auth.py: authorize(): authorization_url created, returning now")
     return authorization_url
 
 def getCredentials(redirect_uri, state, authorization_response):
- 
+    print("auth.py: getCredentials(): getCredentials() called")
+    print("auth.py: getCredentials(): Creating `flow`... ")
+    
     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
         CLIENT_SECRETS_FILE_LOC, scopes=SCOPES, state=state)
     flow.redirect_uri = redirect_uri
 
+    print("auth.py: getCredentials(): `flow` object created, fetching credentials...")
+
     # Use the authorization server's response to fetch the OAuth 2.0 tokens.
     flow.fetch_token(authorization_response=authorization_response)
-
-    # Return credentials in the session.
-    # ACTION ITEM: In a production app, you likely want to save these
-    #              credentials in a persistent database instead.
+    print("auth.py: getCredentials(): OAUTh2 credentials received, returning now")
 
     return credentials_to_dict(flow.credentials)
 
 def revokeAuth(session):
+    print("auth.py: revokeAuth(): revokaAuth() called")
     if 'credentials' not in session:
+        print("NOTE: auth.py: revokeAuth(): Credentials not found, returning False")
         return False
 
     credentials = google.oauth2.credentials.Credentials(
-    **session['credentials'])
-
+        **session['credentials'])
+    
+    print("auth.py: revokeAuth(): Sending revoke request...")
     revoke = requests.post('https://oauth2.googleapis.com/revoke',
         params={'token': credentials.token},
         headers = {'content-type': 'application/x-www-form-urlencoded'})
 
     status_code = getattr(revoke, 'status_code')
     if status_code == 200:
+        print("auth.py: revokeAuth(): Revoke request successful, returning True")
         return True
     else:
+        print("ERROR: auth.py: revokeAuth(): Remote error revoking credentials, status_code=" + status_code + ", returning False")
         return False
 
 
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 # Main application script
 
 import os
+from re import template
 from flask import Flask, render_template, redirect, session, url_for, request
 from google.oauth2 import credentials
-from googleapiclient.discovery import build
+from googleapiclient.discovery import build, Resource
 import auth
+service = False
 
 app = Flask(__name__)
-app.secret_key = "CHANGE ME IN PRODUCTION" # TODO Needed for session 
+app.secret_key = "CHANGE ME IN PRODUCTION" # TODO Needed for session
 
 @app.route("/")
 def index():
@@ -19,7 +21,6 @@ def authorize():
     # Get authorization url to redirect to
     authorization_url, state = auth.authorize(url_for('oauth2callback', 
                                         _external=True))
-
     # Store the state so the callback can verify the auth server response.
     session['state'] = state
 
@@ -34,59 +35,46 @@ def oauth2callback():
         session['state'],
         request.url # authorization_response
     )
+    # ACTION ITEM: In a production app, you likely want to save these
+    #              credentials in a persistent database instead.
 
+    if not session['credentials']:
+        return ("<p>An error has occured</p>")
+     
+    # Build API service object
+    buildService(session['credentials'])
+    
     # Redirect to index
     return redirect(url_for('index'))
 
 @app.route("/revoke")
 def revoke():
-    # Revokes credentials (not working)
-    # FIXME
+    global service
+    # Revokes OATUH access, user must allow application to reaccess API
+    service = False
     revoke = auth.revokeAuth(session)
     if revoke:
         return redirect(url_for('index'))
     else: 
         return ("<p>Error revoking access<br><a href='/'>index</a></p>" + 
-                "<p> You are most likely still logged in </p>")
+                "<p> You were probably not logged in. </p>")
 
 @app.route("/clear")
 def clear():
+    global service
     # Clears creds from session (logs out)
+    service = False
     if 'credentials' in session:
         del session['credentials']
         return redirect(url_for('index'))
     else:
         return ("<p>No stored credentials<br><a href='/'>index</a></p>")
 
-# Labels test
-@app.route("/labels")
-def labels():
-    if 'credentials' not in session:
-        return ("You're not logged in")
-
-    # Create credentials object from credentials dict stored in session
-    creds = credentials.Credentials(**session['credentials'])
-
-    # Build service object for API
-    service = build(
-        auth.API_SERVICE_NAME,
-        auth.API_VERSION, 
-        credentials = creds
-    )
-
-    # API Call
-    results = service.users().labels().list(userId='me').execute()
-    labels = results.get('labels', [])
-
-    if not labels:
-        return ("No labels found")
+@app.route("/playgroud")
+def api_playgroud():
+    labels = getLabels(session['credentials']['client_id'])
     
-    output = "<h1>Labels</h1><br>"
-    for label in labels:
-        output += (label['name'] + ":      " + str(label) + "<br>")
-    
-    output += "<a href='/'>Index</a"
-    return output
+    return render_template('api_playground.html', labels=labels)
 
 @app.route("/start")
 def star():
@@ -128,7 +116,33 @@ def outboxRead():
 def signUp():
     return render_template('signup.html')
 
+# API Methods
+def buildService(session_creds):
+    global service
+    if service:
+        print("main.py: buildService(): buildService() called but service already created")
+        return 
+    print("main.py: buildService(): Building new service resource for API")
+    # Create credentials object from credentials dict stored in session
+    creds = credentials.Credentials(**session_creds)
 
+    # Build service object for API
+    service = build(
+        auth.API_SERVICE_NAME,
+        auth.API_VERSION, 
+        credentials = creds
+    )
+    print("main.py: buildService(): New service resource built")
+
+def getLabels(user_id):
+    # Returns
+    # API Call
+    buildService(session['credentials'])
+    results = service.users().labels().list(userId='me').execute()
+    labels = results.get('labels', [])
+    return labels
+
+# Flask preferences
 if __name__ == "__main__":
     # DO NOT USE IN PRODUCTION
     # DISABLES HTTPS requirement for OAUTH2
@@ -136,4 +150,4 @@ if __name__ == "__main__":
     # TODO enable SSL on httpd server
 
     # Set debug=False in production
-    app.run(debug=True, host='127.0.0.1', port=5000) 
+    app.run(debug=True, host='127.0.0.1', port=5000)
